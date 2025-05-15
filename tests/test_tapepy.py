518393d878d0803cf91387c18efda2dd6df1e123
Mlezcek <116952662+Mlezcek@users.noreply.github.com>
2025-05-15 23:52:43 +0200

    Cosmetic change

--- tests/test_tapepy.py
+++ tests/test_tapepy.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-# Dodajemy główny katalog do sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from tapepy import core, export, stats, tape
